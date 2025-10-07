dfbf8a7367d32b6361dd734c2d92de964fe29884
Tushar Sharma <tusharsharma.techsavvy@gmail.com>
2025-10-07 19:37:21 -0400

    Fix path error by correcting slash in configuration

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "bucket-name" # WILL ADD UP AFTER COMPLETING ROOT MODULE
-    key            = "/terraform.tfstate"    
+    key            = "terraform.tfstate"
     region         = "us-east-1" # CURRENT REGION                    
     dynamodb_table = "dynamo-db" # WILL ADD UP AFTER COMPLETING ROOT MODULE AS IT COSTS
     encrypt        = true
